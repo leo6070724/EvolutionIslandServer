740e1c67c072a0fffb804e469a8f679804cfe6cb
leo6070724 <leo6070724@gmail.com>
2017-09-10 18:06:40 +0800

    .

---
+++ opt/app-root/src/Interface/IGameManager.cs
@@ -0,0 +1,16 @@
+﻿using EvolutionislandGameServer.DTO;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvolutionislandGameServer.Interface
+{
+    public interface IGameManager
+    {
+        object Locking { get; }
+        ConcurrentDictionary<string, Player> PlayerMap { get; }
+
+        string GetAppVersion();
+    }
+}
