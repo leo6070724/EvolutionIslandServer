f7a0aaefd319545d6d6cd094d99052855eba50f4
leo6070724 <leo6070724@gmail.com>
2017-09-10 17:57:28 +0800

    .

---
+++ GameMethod/GameManager.Initialize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvolutionislandGameServer.GameMethod
+{
+    public partial class GameManager
+    {
+        /// <summary>
+        /// 初始化遊戲
+        /// </summary>
+        public void InitializeGame()
+        {
+
+        }
+    }
+}
---
+++ SocketServer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace EvolutionislandGameServer
+{
+    public class SocketServer
+    {
+        static ConcurrentDictionary<int, Client> ClientMap;
+        static private int CurrentPlayerNum { get; set; }
+        static private Socket mServer;
+        public SocketServer()
+        {
+            mServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ClientMap = new ConcurrentDictionary<int, Client>();
+        }
+        public void Start(IPAddress ipAddress, int port)
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+            mServer.Bind(ipEndPoint);
+            mServer.Listen(50);
+            Task.Run(()=>
+            {
+                while(true)
+                {
+                    try
+                    {
+                        Socket acceptedSocket = mServer.Accept();
+                        CurrentPlayerNum++;
+                        Client aClient = new Client(CurrentPlayerNum, acceptedSocket);
+                        ClientMap.TryAdd(CurrentPlayerNum, aClient);
+                        BoardCast("123");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Stop();
+                    }
+                }                
+            });
+            
+        }
+        internal static void BoardCast(string v)
+        {
+            foreach (var aClient in ClientMap.ToArray())
+            {
+                aClient.Value.Send(v);
+            }
+        }
+
+        internal static void RemoveClient(int currentPlayerNum, Client client)
+        {
+            if (ClientMap.ContainsKey(currentPlayerNum))
+                ClientMap.TryRemove(currentPlayerNum, out client);
+        }
+        public void Stop()
+        {
+            mServer.Dispose();
+        }
+    }
+}
